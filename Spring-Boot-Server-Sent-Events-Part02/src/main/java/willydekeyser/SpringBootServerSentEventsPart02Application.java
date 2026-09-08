40efce35cb31b00fab724e5db385246c6412a003
Willy De Keyser <spring.boot.programming@gmail.com>
2026-09-08 12:30:31 +0200

    Init

---
+++ Spring-Boot-Server-Sent-Events-Part02/src/main/java/willydekeyser/SpringBootServerSentEventsPart02Application.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServerSentEventsPart02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootServerSentEventsPart02Application.class, args);
+	}
+
+}
---
+++ Spring-Boot-Server-Sent-Events-Part02/src/test/java/willydekeyser/SpringBootServerSentEventsPart02ApplicationTests.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootServerSentEventsPart02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
